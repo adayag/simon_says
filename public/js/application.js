9d2dfd69251ca462061269965d7d7096514efb73
Allen Dayag <adayag@Allens-MacBook-Pro.local>
2013-07-25 23:15:49 -0500

    last_commit

--- public/js/application.js
+++ public/js/application.js
@@ -1,3 +1,9 @@
 $(document).ready(function(){
-
+  $('#get_color').on('click', function(event){
+  	event.preventDefault();
+  	var url = ('/color')
+  	$.post(url, function(response){
+  		$('#color_me li:nth-child('+response.cell+')').css('background-color', response.color)
+  	}, 'json');
+  });
 });
